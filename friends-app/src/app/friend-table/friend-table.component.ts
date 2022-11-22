4985365bfab7935d6497530614b0b17e831ff36b
hunter johnson <chefhj1000@aol.com>
2022-11-22 13:23:11 -0600

    changes to friend table

--- friends-app/src/app/friend-table/friend-table.component.ts
+++ friends-app/src/app/friend-table/friend-table.component.ts
@@ -1,33 +1,50 @@
-import {Component, Input,} from '@angular/core';
+import {Component, Inject, Input,} from '@angular/core';
 import {Friend} from "../interaces/friend";
 import {MatTableDataSource} from "@angular/material/table";
 import {SelectionModel} from "@angular/cdk/collections";
+import {Store} from "@ngrx/store";
+import {updateFriendList} from "../+state/app.actions";
+import {EditFormComponent} from "../edit-form/edit-form.component";
+import {MAT_DIALOG_DATA, MatDialog} from "@angular/material/dialog";
 @Component({
   selector: 'app-friend-table',
   templateUrl: './friend-table.component.html',
   styleUrls: ['./friend-table.component.scss']
 })
 export class FriendTableComponent {
-  @Input() set data(data: any) {
+  @Input() set dataSet(data: any) {
     if (data) {
       this.dataSource = new MatTableDataSource<Friend>(data);
+      this.dataInput = data;
     }
   };
+  dataInput: Friend[];
   displayedColumns: string[] = ['select', 'name', 'weight', 'age', 'friends'];
   dataSource = new MatTableDataSource<Friend>([]);
   selection = new SelectionModel<Friend>(false, []);
+  constructor (private store: Store,
+               private dialog: MatDialog,
+          ){ }
+  deleteFriend() {
+    if (this.selection.selected?.[0]) {
+      const index = this.dataInput.indexOf(this.selection.selected[0]);
+      const tempData = [...this.dataInput];
+      tempData.splice(index, 1);
+      this.dataInput = tempData;
+      this.dataSource = new MatTableDataSource<Friend>(this.dataInput);
+      this.selection = new SelectionModel<Friend>(false, []);
+      this.store.dispatch(updateFriendList({payload: this.dataInput}));
+    }
 
-  /** Whether the number of selected elements matches the total number of rows. */
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length;
-    return numSelected === numRows;
+  }
+  editFriend() {
+    const dialogRef = this.dialog.open(EditFormComponent, {
+      width: '450px',
+      height: '400px',
+      data: {selectedFriend: this.selection.selected[0], friends: this.dataInput}});
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 
-  /** Selects all rows if they are not all selected; otherwise clear selection. */
-  masterToggle() {
-    this.isAllSelected() ?
-      this.selection.clear() :
-      this.dataSource.data.forEach(row => this.selection.select(row));
-  }
 }
