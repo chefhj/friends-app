443526e7d25ef8ed3e98322f81cc18c384b4b006
hunter johnson <chefhj1000@aol.com>
2022-11-22 08:13:34 -0600

    built out input form

--- friends-app/src/app/input-form/input-form.component.ts
+++ friends-app/src/app/input-form/input-form.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import {ValidationError} from "json-schema";
 import {Friend} from "../interaces/friend";
 import {Store} from "@ngrx/store";
 import {addFriend} from "../+state/app.actions";
+import {initialFriends} from "../mockdata/initial-friends";
 
 @Component({
   selector: 'app-input-form',
@@ -11,12 +11,13 @@ import {addFriend} from "../+state/app.actions";
   styleUrls: ['./input-form.component.scss']
 })
 export class InputFormComponent implements OnInit {
+  @Input() friends: Friend[] = initialFriends;
   startDate = new Date(1990, 0, 1);
   friendForm: FormGroup | any = {};
   nameFormControl: FormControl = new FormControl<any>('', [Validators.required, Validators.minLength(3), Validators.pattern("\\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+")]);
   weightFormControl: FormControl = new FormControl<any>('', [Validators.required, Validators.minLength(2), Validators.pattern("^[0-9]*$"),]);
   ageFormControl: FormControl = new FormControl<any>('', [Validators.required],);
-
+  friendFormControl = new FormControl([]);
   constructor(
     private fb: FormBuilder,
     private store$: Store
@@ -32,14 +33,25 @@ export class InputFormComponent implements OnInit {
       }
     )
   }
-
-  addFriend() {
+  createFriend() {
     const newFriend: Friend = {
       name: this.nameFormControl.value,
-      age: this.ageFormControl.value,
+      age: new Date(Date.now()).getFullYear() - this.ageFormControl.value?.getFullYear(),
+      dob: this.ageFormControl.value,
       weight: this.weightFormControl.value,
-      friends: [],
+      friends: this.friendFormControl.value,
     };
-    return this.store$.dispatch(addFriend({payload: newFriend}));
+    return newFriend;
+  }
+  clearForm() {
+    this.nameFormControl.reset();
+    this.ageFormControl.reset();
+    this.weightFormControl.reset();
+    this.friendFormControl.reset();
+  }
+  addFriend() {
+    const newFriend = this.createFriend();
+    this.store$.dispatch(addFriend({payload: newFriend}));
+    this.clearForm();
   }
 }
