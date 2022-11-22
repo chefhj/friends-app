d24c5f8583bbab83328cead08d6dff1bd0769f76
hunter johnson <chefhj1000@aol.com>
2022-11-22 08:12:52 -0600

    added routes

--- friends-app/src/app/app-routing.module.ts
+++ friends-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LandingPageComponent} from "./landing-page/landing-page.component";
+import {VisualizationLayoutComponent} from "./visualization-layout/visualization-layout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'visualizations', component: VisualizationLayoutComponent },
+  { path: 'home', component: LandingPageComponent },
+  { path: '**', component: LandingPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
