02717a0ac03f2b03a2e7f308ce0a619eadd2efaf
hunter johnson <chefhj1000@aol.com>
2022-11-22 08:14:08 -0600

    boiler

--- friends-app/src/app/app.module.ts
+++ friends-app/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,18 +7,30 @@ import { InputFormComponent } from './input-form/input-form.component';
 import { MaterialModule } from "../../projects/material/src/lib/material.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { StoreModule } from '@ngrx/store';
+import { FriendTableComponent } from './friend-table/friend-table.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import {appReducer, initialState} from "./+state/app.reducer";
+import { FriendBarGraphsComponent } from './friend-bar-graphs/friend-bar-graphs.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { FriendScatterPlotComponent } from './friend-scatter-plot/friend-scatter-plot.component';
+import { VisualizationLayoutComponent } from './visualization-layout/visualization-layout.component';
 @NgModule({
   declarations: [
     AppComponent,
-    InputFormComponent
+    InputFormComponent,
+    FriendTableComponent,
+    LandingPageComponent,
+    FriendBarGraphsComponent,
+    AppHeaderComponent,
+    FriendScatterPlotComponent,
+    VisualizationLayoutComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
     AppRoutingModule,
-    StoreModule.forRoot({app: appReducer}, {initialState: {app: initialState}}),
+    StoreModule.forRoot({app: appReducer}, {initialState: {app: initialState}})
   ],
   providers: [],
   bootstrap: [AppComponent]
